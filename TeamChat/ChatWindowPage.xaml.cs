5dbf8e608570ed68d1f270029f1fcea4bfbe11c7
Shahid Aman <aman.shahid@gmail.com>
2017-03-06 00:28:51 +0100

    * .gitignore: Initial commit
    * Main.cs:
    * TeamChat.sln:
    * Info.plist:
    * App.xaml:
    * AppDelegate.cs:
    * packages.config:
    * App.xaml.cs:
    * MainActivity.cs:
    * packages.config:
    * Entitlements.plist:
    * TeamChat.iOS.csproj:
    * TeamChat.csproj:
    * packages.config:
    * SignupPage.xaml:
    * TeamChatPage.xaml:
    * TeamChat.Droid.csproj:
    * SignupPage.xaml.cs:
    * LaunchScreen.storyboard:
    * ChatWindowPage.xaml:
    * AboutAssets.txt:
    * TeamChatPage.xaml.cs:
    * ChatWindowPage.xaml.cs:
    * AssemblyInfo.cs:
    * Contents.json:
    * icon.png:
    * styles.xml:
    * Tabbar.axml:
    * AboutResources.txt:
    * AssemblyInfo.cs:
    * Toolbar.axml:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * Contents.json:

---
+++ TeamChat/ChatWindowPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Xamarin.Forms;
+
+namespace TeamChat
+{
+	public partial class ChatWindowPage : ContentPage
+	{
+		private static HttpClient httpClient;
+		private static string ApiKey;
+		public ChatWindowPage(string apiKey)
+		{
+			InitializeComponent();
+			httpClient = new HttpClient();
+			ApiKey = apiKey;
+			messageEditor.Completed +=  (object sender, EventArgs e) =>
+			{
+				messageLabel.Text = TranslateToEnglish(messageEditor.Text, ApiKey);
+			};
+		}
+
+		private string TranslateToEnglish(string inputText, string apiKey)
+		{ 
+			httpClient.BaseAddress = new Uri("https://translation.googleapis.com/language/translate/v2");
+			var result = httpClient.GetAsync("?key=" + apiKey + "&target=" + "en" + "&q=" + inputText).Result;
+			var jsonString =  result.Content.ReadAsStringAsync().Result;
+
+			var text = JObject.Parse(jsonString).SelectToken("data").SelectToken("translations")[0].SelectToken("translatedText").Value<string>();
+			return text;
+
+		}
+	}
+}
---
+++ TeamChat/TeamChatPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TeamChat
+{
+	public partial class TeamChatPage : ContentPage
+	{
+		public TeamChatPage()
+		{
+			InitializeComponent();
+
+			Device.OnPlatform(
+				iOS: () =>
+			{
+				Padding = new Thickness(0, 20, 0, 0);
+			});
+
+			NavigationPage.SetHasNavigationBar(this, false);
+			buttonLogin.Clicked += async (object sender, EventArgs e) =>
+			{
+				loginActivityIndicator.IsRunning = true;
+				await Navigation.PushAsync(new ChatWindowPage(apiKeyEntry.Text));
+				loginActivityIndicator.IsRunning = false;
+			};
+
+			buttonSignUp.Clicked += async (object sender, EventArgs e) =>
+			{
+				await Navigation.PushAsync(new SignupPage());
+			};
+		
+		}
+	}
+}
